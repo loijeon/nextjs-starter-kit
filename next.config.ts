18c063bb98699ef58fca6d9d9cb9a0ca5944e839
loijeon <kumlang@gmail.com>
2026-05-20 11:41:01 +0900

    *fix: nextjs-starter-kit ip접근시 다크/라이트모드 기능 안되는 것 수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "localhost",
+    "127.0.0.1",
+    "10.53.40.166",
+  ],
 };
 
 export default nextConfig;
